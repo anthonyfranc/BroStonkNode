fd887cf0c1a135b4075701a1f7e55ad785779eaf
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-09-08 08:47:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function stopCheckApiInterval() {
 // Function to run checkApi() every 10 minutes when there are no active connections
 function startNoConnectionInterval() {
   if (!isWebSocketActive) {
-    noConnectionInterval = setInterval(checkApi, 600000); // 10 minutes interval
+    noConnectionInterval = setInterval(checkApi, 60000); // 10 minutes interval
   }
 }
 
